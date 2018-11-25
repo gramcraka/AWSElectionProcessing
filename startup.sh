ebc0d927d4ed53b61db373ff8c483e946a339a9e
gramcraka <ethang@smu.edu>
2018-11-25 12:22:31 -0800

    Install Jupyter Notebook commands.

--- startup.sh
+++ startup.sh
@@ -8,3 +8,8 @@ unzip anes_timeseries_cdf.zip
 sudo yum install -y python36
 python3 -m venv election && source election/bin/activate && pip install -U scikit-learn pandas matplotlib
 
+# Install Jupyter: http://jupyter.org/install
+sudo python3 -m pip install --upgrade pip
+sudo python3 -m pip install jupyter
+
+  
